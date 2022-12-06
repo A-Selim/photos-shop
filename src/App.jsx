cca46978b6dcb04109368a9678d58205cd080ede
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2022-12-06 16:31:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
