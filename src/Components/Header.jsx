10bf1db01487250d32cf14d6787dc6460b50bc77
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2022-12-13 15:46:25 +0200

    Add cart icon to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { PhotosContext } from "../../src/PhotosContext";
 
 export default function Header() {
+  const { cart } = useContext(PhotosContext);
+
   return (
     <nav className="navbar">
       <Link to="/" className="page-link">
         Photos
       </Link>
       <Link to="/cart" className="page-link">
-        Cart
+        <i className={`cart-icon-header ri-shopping-cart-2-${cart.length > 0 ? "fill" : "line"}`}></i>
       </Link>
     </nav>
   );
